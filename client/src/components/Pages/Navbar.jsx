ae7ada5c7dc75a2f4edf38fb2c1063c2fd3c225d
Jay <jaypatel0523@gmail.com>
2023-03-10 09:02:48 +0530

    middleware added

--- client/src/components/Pages/Navbar.jsx
+++ client/src/components/Pages/Navbar.jsx
@@ -2,6 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+<<<<<<< Updated upstream
+=======
+  const navigateTo = useNavigate();
+  const handleMenu = () => {
+    navigateTo("/menu");
+  };
+
+>>>>>>> Stashed changes
   return (
     <>
       <div>This is navbar</div>
--- server/routes/product.js
+++ server/routes/product.js
@@ -2,10 +2,11 @@ const epxress = require('express');
 const router = epxress.Router();
 const { sellProduct, } = require('../controllers/sellProduct');
 const { getAllProducts, getProductCategorywise, getProductStatewise, getProductCitywise } = require('../controllers/product');
+const authentication = require('../middleware/authentication');
 
 
 router.route('/sellProduct').post(sellProduct);
-router.route('/getAllProducts').get(getAllProducts);
+router.route('/getAllProducts').get(authentication, getAllProducts);
 router.route('/getProductCategorywise/:category').get(getProductCategorywise);
 router.route('/getProductStatewise/:state').get(getProductStatewise);
 router.route('/getProductCitywise/:city').get(getProductCitywise);
